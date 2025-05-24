9fc637db35033475cdc058a769b8ad4d39170b5b
Saptak-Guha <saptakguha1454@gmail.com>
2025-05-25 02:02:15 +0530

    Fronted modified /login added

--- env/Scripts/django-admin.py
+++ env/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Subha\self_proj\b2s\client_info\env\Scripts\python.exe
+#!C:\Users\KIIT\Documents\GitHub\client_info\env\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
